6f720a86750a9487fdaa9913018bd50a65bb96ab
Jakob Luettgau <luettgau@dkrz.de>
2016-07-27 17:10:27 +0200

    add backup manager workload generator stub

---
+++ tapesim/workloads/BackupManager.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+
+
+class BackupManager(object):
+    def __init__(self):
+        """
+
+        full: 
+            a full copy of the data
+            
+            + quick recovery
+            - high capacity required
+
+
+        cumulative incremental:
+            
+            + less capacity required
+            ~ slightly slower recovery
+
+        differential incremental:
+           
+           - slow recovery process
+
+
+        incremental forever backup:
+           
+            + no redundant data transmitted from clients to storage, lowest capacity demands
+            - not feasable with tape, but ok good with disks
+            - highly vulnerable to media errors
+
+
+
+        usually hybrid strategies are used
+
+
+
+        """
+
+
+            
+
+        self.unit  = "days"
+        self.pivot = "sunday"
+
+        #                         SmtwtfsSmtwtfsSmtwtfsSmtwtfsSmtwtfsSmtwtfsSmtwtfsSmtwtfsSmtwtfsS
+        self.strategy_sequence = "fddddddcddddddcddddddcdddddd" # full every month, cummulative weekly, incremental in between
+
+
+
+        pass
